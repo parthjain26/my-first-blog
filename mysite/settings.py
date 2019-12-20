0250daa61695f3bc35309510ca49b621ed6eb4b7
Your Name <you@example.com>
2019-12-20 17:51:00 +0530

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'slg8%fx#%rk-+v@mexc)bo134y=$#c!dkx-*dmo=i25k2e*ji4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com', 'djangoblog26.pythonanywhere.com']
 
 
 # Application definition
